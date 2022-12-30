6c5a8eb9be1aefd5d75c711fdb868ab0a5d5ba23
Sergey B <brazhnikov_serge@mail.ru>
2022-12-30 15:44:29 +0300

    add new file-variant2

---
+++ variant2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = CreateArray(size);
+Console.WriteLine($"Введенный массив: [{string.Join(",", array)}]");
+Console.WriteLine($"[{ShowSomeElements(array)}]");
+string[] CreateArray(int sizeArray)
+{
+    string[] newArray = new string[sizeArray];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        Console.WriteLine("Введите элемент:");
+        newArray[i] = Console.ReadLine();
+    }
+    return newArray;
+}
+string ShowSomeElements(string[] inputArray)
+{
+    string str = "";
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+            str += (inputArray[i] + ", ");
+    }
+    str = str.Remove(str.Length - 2);
+    return str;
+}
