62098b0c6d4bb1dcd65c5606f13fe07b56c14d06
Sergey B <brazhnikov_serge@mail.ru>
2022-12-30 15:41:17 +0300

    add new file-variant1

---
+++ variant1/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = CreateArray(size);
+Console.WriteLine($"Введенный массив: [{string.Join(",", array)}]");
+Console.WriteLine($"Введенный массив: [{string.Join(",", ShowSomeElements(CountElementsInCondition(array),array))}]");
+string[] CreateArray(int sizeArray)
+{
+    string[] newArray = new string[sizeArray];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        Console.WriteLine("Введите элемент:");
+        newArray[i] = Console.ReadLine();
+    }
+    return newArray;
+}
+int CountElementsInCondition(string[] inputArray)
+{
+    int count = 0;
+    string[] arr = new string[count];
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+string[] ShowSomeElements(int countnumber,string[] inputArray)
+{
+    string[] resultArr = new string[countnumber];
+    int count=0; 
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            resultArr[count]=inputArray[i];
+            count++;
+        }
+    }
+   
+    return resultArr;
+}
